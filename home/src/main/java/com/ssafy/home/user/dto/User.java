b83370384c324957885dd511b1bdd8442cd4e9d9
stoneTiger0912 <seokbeom0912@gmail.com>
2024-05-09 23:59:45 +0900

    Feat: Add UserDto

---
+++ home/src/main/java/com/ssafy/home/user/dto/User.java
@@ -0,0 +1,11 @@
+package com.ssafy.home.user.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String id;
+    private String password;
+    private String email;
+    private String nickname;
+}
