05d4924af5f0fcc417d89738be1e951acf4b559c
stoneTiger0912 <seokbeom0912@gmail.com>
2024-05-10 00:00:20 +0900

    Feat: Add UserRepository

---
+++ home/src/main/java/com/ssafy/home/user/model/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.ssafy.home.user.model.mapper;
+
+import com.ssafy.home.user.dto.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper {
+    //회원 가입
+    int regist(User user);
+
+    //로그인
+    User login(User user);
+
+    //중복 조회
+    int findUser(User user);
+
+    //내정보 조회
+    User findById(User user);
+
+    //내 정보 수정
+    int updateUser(User user);
+
+    //회원 탈퇴
+    int deleteUser(String id);
+
+}
