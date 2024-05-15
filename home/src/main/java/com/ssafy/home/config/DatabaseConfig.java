b15b0387e05afe68b8a9261bce202443ffb7104a
stoneTiger0912 <seokbeom0912@gmail.com>
2024-05-15 20:51:46 +0900

    Feat: Fix database

---
+++ home/src/main/java/com/ssafy/home/config/DatabaseConfig.java
@@ -0,0 +1,10 @@
+package com.ssafy.home.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages = "com.ssafy.home.*.model.mapper")
+public class DatabaseConfig {
+
+}
