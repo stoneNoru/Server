ac42e7861459941fa638e011c8d9fce07e2b7147
stoneTiger0912 <seokbeom0912@gmail.com>
2024-05-15 20:51:21 +0900

    Feat: Add home

---
+++ home/src/main/java/com/ssafy/home/apt/dto/AddressDto.java
@@ -0,0 +1,11 @@
+package com.ssafy.home.apt.dto;
+
+import lombok.Data;
+
+@Data
+public class AddressDto {
+	private String dongCode;
+	private String sidoName;
+	private String gugunName;
+	private String dongName;
+}
---
+++ home/src/main/java/com/ssafy/home/apt/dto/DealDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.home.apt.dto;
+
+import lombok.Data;
+
+@Data
+public class DealDto {
+	private long no;
+	private String dongCode;
+	private String dealAmount;
+	private int dealYear;
+	private int dealMonth;
+	private int dealDay;
+	private String floor;
+	private String area;
+	private String apartmentName;
+	private long aptCode;
+	private String lng;
+	private String lat;
+	
+}
